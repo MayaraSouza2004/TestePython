ce93eb7a8aa8320c012e2f6961e456209c98080b
MayaraSouza2004 <mayaradesouza731@gmail.com>
2023-12-08 11:27:33 -0300

    Create testepython.txt.py
    Subindo minha primeira atividade da lista

---
+++ Lista de Exercício/testepython.txt.py
@@ -0,0 +1,7 @@
+nome= (input("Informe o nome \n"))
+nota1=int(input("Informe a 1° nota \n"))
+nota2=int(input("Informe a 2° nota \n"))
+nota3=int(input("Informe a 3° nota \n"))
+media=(nota1+nota2+nota3)/3
+print("A sua média aritmética:",media)
+
